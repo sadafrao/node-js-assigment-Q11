2c1c1c6b0af5d5c32b360b1a6d1f8e7334740d39
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:34:38 -0800

    Add files via upload

---
+++ Q11.js
@@ -0,0 +1,6 @@
+// 11. Names : Store the names of a few of Your Friends in a array called names. Print each persons name by accessing each element in the list , one at a time.
+var names = ["Ali", "fatima", "sanam", "sana"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
